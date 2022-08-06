a78d4ec813fc986a38bb75f974ff4b71c58a7f76
liushnya <eliushnya@gmail.com>
2022-08-06 19:57:34 +0300

    Added words quantity calculation

--- viceversa/views.py
+++ viceversa/views.py
@@ -6,6 +6,8 @@ def home(request):
 
 def reverse(request):
 	user_text = request.GET['usertext']
+	words_quantity = len(user_text.split())
 	reversed_text = user_text[::-1]
 	# print(test_get_text)
-	return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext': reversed_text})
+	return render(request, 'reverse.html', {'usertext': user_text, 'reversedtext': reversed_text,
+		'wordsquantity': words_quantity})
